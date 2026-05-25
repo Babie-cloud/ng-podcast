2117f7796df4638e1456cac862f882497c05093d
Babie-cloud <karliegionacubahiro@gmail.com>
2026-05-26 00:01:30 +0200

    add localize

--- ng-podcast/src/main.ts
+++ ng-podcast/src/main.ts
@@ -1,7 +1,8 @@
+/// <reference types="@angular/localize" />
+
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';   // ← correspond à "export class App"
+import { App } from './app/app'; // ← correspond à "export class App"
 
-bootstrapApplication(App, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
