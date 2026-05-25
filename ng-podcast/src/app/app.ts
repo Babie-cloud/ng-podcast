0c3c47276e60ffb38980f683d3885719f7b2c2c9
Babie-cloud <karliegionacubahiro@gmail.com>
2026-05-25 23:35:47 +0200

    footer

--- ng-podcast/src/app/app.ts
+++ ng-podcast/src/app/app.ts
@@ -3,17 +3,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/pages/navbar/navbar';
 import { Player } from './features/podcast/components/player/player';
+import { Footer } from './features/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Player],
+  imports: [RouterOutlet, Navbar, Player, Footer],
   template: `
     <app-navbar />
     <main>
       <router-outlet />
     </main>
     <app-player />
+    <app-footer />
   `
 })
 export class App {}
