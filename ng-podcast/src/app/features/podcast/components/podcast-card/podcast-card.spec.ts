08347b0657bf00ac90c7917254fe1a5a8a3be9b0
Babie-cloud <karliegionacubahiro@gmail.com>
2026-05-27 16:55:59 +0200

    new components

---
+++ ng-podcast/src/app/features/podcast/components/podcast-card/podcast-card.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PodcastCard } from './podcast-card';
+
+describe('PodcastCard', () => {
+  let component: PodcastCard;
+  let fixture: ComponentFixture<PodcastCard>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PodcastCard],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PodcastCard);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- ng-podcast/src/app/features/podcast/components/podcast-list/podcast-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-podcast-list',
-  imports: [],
-  templateUrl: './podcast-list.html',
-  styleUrls: ['./podcast-list.scss'] ,  
-})
-export class PodcastList {}
