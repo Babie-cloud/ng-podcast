0c471338ff2f1646c967d4420d8be4bef8301082
Babie-cloud <karliegionacubahiro@gmail.com>
2026-05-28 01:31:41 +0200

    google auth

--- ng-podcast/src/environments/environment.ts
+++ ng-podcast/src/environments/environment.ts
@@ -2,6 +2,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8080',
-  googleClientId:
-    '133671696747-vm4qm6fmbl8rul31pb42lcbpr089n0m1.apps.googleusercontent.com',
+  googleClientId: '',
 };
