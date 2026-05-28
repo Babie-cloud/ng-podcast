d12e18225c4e1734891c7565c7a2fa7161b9b2e1
Babie-cloud <karliegionacubahiro@gmail.com>
2026-05-29 01:35:53 +0200

    initial commit

--- ng-podcast/src/environments/environment.prod.ts
+++ ng-podcast/src/environments/environment.prod.ts
@@ -2,6 +2,9 @@
 // Exemple : https://api.votredomaine.com  ou  https://votredomaine.com si l’API est derrière le même host.
 export const environment = {
   production: true,
+  // Remplacez par l'URL publique de votre API Render (sans slash final).
   apiUrl: 'https://CHANGE_ME_BACKEND_PUBLIC_URL',
-  googleClientId: 'CHANGE_ME_GOOGLE_CLIENT_ID',
+  // Le Google Client ID n'est PAS secret : il est public cote navigateur.
+  googleClientId:
+    '133671696747-vm4qm6fmbl8rul31pb42lcbpr089n0m1.apps.googleusercontent.com',
 };
